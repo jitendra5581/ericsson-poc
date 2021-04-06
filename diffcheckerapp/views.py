fca0ea348bad0019fd910e03eee8ac07dfe811fe
jitendra <jitendra5581>
2021-04-06 11:12:03 +0530

    log scanner added

--- diffcheckerapp/views.py
+++ diffcheckerapp/views.py
@@ -7,18 +7,21 @@ import time
 from django.http import HttpResponse
 
 from .models import *
+from utilities.switchlogs import *
 
 
 def get_diff(file1 , file2):
-    file1_path = os.path.join(settings.LOGS_URL, file1)
-    file2_path = os.path.join(settings.LOGS_URL, file2)
-    file1 = open(file1_path, 'r').readlines()
-    file2 = open(file2_path, 'r').readlines()
+    # file1_path = os.path.join(settings.LOGS_URL, file1)
+    # file2_path = os.path.join(settings.LOGS_URL, file2)
+    file1_path = file1
+    file2_path = file2
+    file1_contents = open(file1_path, 'r').readlines()
+    file2_contents = open(file2_path, 'r').readlines()
     # compare = difflib.HtmlDiff().make_file(fromlines=file1, tolines=file2,
     #                                            fromdesc=(datetime.date.today() - datetime.timedelta(
     #                                                days=1)).isoformat(),
     #                                            todesc=datetime.date.today().isoformat())
-    compare = difflib.unified_diff(file1, file2)
+    compare = difflib.unified_diff(file1_contents, file2_contents)
     deleted_records = list()
     added_records = list()
     for comp in compare:
@@ -45,12 +48,38 @@ def scan_view(request):
         primary_device_id = request.POST.get('primary_device')
         print('pid>>', primary_device_id)
         qs = PrimaryInterface.objects.filter(id=primary_device_id)
+
+        #*********primary device details ***********
         primary_device_ip = qs.first().ip_address
+        prim_device_type = qs.first().device_type
+        prim_username = qs.first().username
+        prim_password = qs.first().password
+        prim_secret = qs.first().secret
+
+        # ******************** logs scanner
+        log_obj = GenerateLogs()
+        primary_log_file_path = log_obj.primary_log(prim_device_type, 
+                                    primary_device_ip, prim_username,
+                                    prim_password, prim_secret
+                                    )
+
         qs2 = SecondaryInterface.objects.get(primary_interface__id=primary_device_id)
+
+        #************ sec. device details **************
         sec_device_ip = qs2.ip_address
-        deleted_records, added_records, file1, file2 = get_diff('device1.text', 'device2.txt')
+        sec_device_type = qs2.device_type
+        sec_username = qs2.username
+        sec_password = qs2.password
+        sec_secret = qs2.secret
+
+        sec_log_file_path = log_obj.secondary_log(sec_device_type, 
+                                    sec_device_ip, sec_username, 
+                                    sec_password, sec_secret
+                                    )
         
-            
+        #deleted_records, added_records, file1, file2 = get_diff('device1.text', 'device2.txt')
+        deleted_records, added_records, file1, file2 = get_diff(primary_log_file_path, sec_log_file_path)
+    
     context = {
         'device_objs' : qs,
         'deleted_records': deleted_records,
@@ -59,7 +88,6 @@ def scan_view(request):
         'secordary_device_logs' : file2,
         'primary_device_ip' : primary_device_ip,
         'sec_device_ip' : sec_device_ip
- 
     }    
     return render(request, 'diffcheckerapp/diff.html', context)
 
---
+++ utilities/switchlogs.py
@@ -0,0 +1,50 @@
+import difflib
+import datetime
+from netmiko import ConnectHandler
+from django.conf import settings
+import os
+
+
+command = 'show running'
+
+class GenerateLogs:
+    primary_log_file_path = ''
+    secondary_log_file_path = '' 
+    def primary_log(self, device_type, ip_address, username, password, secret):
+        session = ConnectHandler(
+                                device_type=device_type, 
+                                ip=ip_address, username=username,
+                                password=password, secret=secret
+                                )
+
+        enable = session.enable()
+        output = session.send_command(command)
+        log_file_path = os.path.join(settings.LOGS_URL, 'primary_logs')
+        file_name = 'primary_'+ip_address+'_'+datetime.date.today().isoformat()
+        self.primary_log_file_path = os.path.join(log_file_path,file_name)
+
+        with open(self.primary_log_file_path, 'w') as primary_log:
+            primary_log.write(output + '\n')
+        primary_log.close() 
+        return self.primary_log_file_path
+    
+    def secondary_log(self, device_type, ip_address, username, password, secret):
+        session = ConnectHandler(
+                                device_type=device_type, 
+                                ip=ip_address, username=username,
+                                password=password, secret=secret
+                                )
+
+        enable = session.enable()
+        output = session.send_command(command)
+        log_file_path = os.path.join(settings.LOGS_URL, 'secondary_logs')
+        file_name = 'secondary_'+ip_address+'_'+datetime.date.today().isoformat()
+        self.secondary_log_file_path = os.path.join(log_file_path,file_name)
+
+        with open(self.secondary_log_file_path, 'w') as secondary_log:
+            secondary_log.write(output + '\n')
+        secondary_log.close()  
+        return self.secondary_log_file_path
+
+
+
